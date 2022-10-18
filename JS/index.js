a9bc3559886ec356686b884a10c4f5f0d458416f
Lionbull <geo1529@yahoo.com>
2022-10-18 23:20:15 +0300

    Some thing added to create function

--- JS/index.js
+++ JS/index.js
@@ -1,7 +1,17 @@
+//Source for 2D models: https://opengameart.org/content/animated-skeleton
+//https://opengameart.org/content/animated-warrior
+
+
 const config = {
     type: Phaser.AUTO,
-    width: 800,
-    height: 600,
+    scale: {
+        mode: Phaser.Scale.FIT,
+        parent: 'phaser-example',
+        autoCenter: Phaser.Scale.CENTER_BOTH,
+        width: 800,
+        height: 600,
+    },
+
     physics: {
         default: "arcade",
         arcade: {
@@ -15,34 +25,77 @@ const config = {
     }
 };
 
+let player;
+let platforms;
+let enemy;
+let score;
+
 const game = new Phaser.Game(config);
 
 function preload () {
-    this.load.setBaseURL('http://labs.phaser.io');
 
-    this.load.image("sky", "assets/skies/clouds.png");
-    this.load.image("logo", "assets/sprites/phaser3-logo.png");
-    this.load.image("red", "assets/particles/red.png");
+
+    this.load.image("sky", "../assets/sky.png");
+    this.load.image("platform", "../assets/platform.png")
+    this.load.image("ground", "../assets/ground.png")
+    this.load.spritesheet({
+        key: 'player',
+        url: '../assets/warrior.png',
+        frameConfig: {
+            frameWidth: 32,
+            frameHeight: 32
+        }
+    });
+    this.load.spritesheet({
+        key: 'enemy',
+        url: '../assets/skeleton.png',
+        frameConfig: {
+            frameWidth: 32,
+            frameHeight: 32
+        }
+    });
 }
 
 function create () {
     this.add.image(400, 300, "sky");
 
-    const particles = this.add.particles("red");
+    enemy = this.physics.add.image(200, 100, "enemy");
+    enemy.displayWidth = 48
+    enemy.displayHeight = 48
 
-    const emitter = particles.createEmitter({
-        speed: 100,
-        scale: { start: 1, end: 0},
-        blendMode: "ADD"
-    });
+    enemy.setVelocity(100);
+    enemy.setBounce(0.2);
+    enemy.setCollideWorldBounds(true)
 
-    const logo = this.physics.add.image(400, 100, "logo");
+    // Creating the ground
+    platforms = this.physics.add.staticGroup();
 
-    logo.setVelocity(100, 200);
-    logo.setBounce(1, 1);
-    logo.setCollideWorldBounds(true);
+    platforms.create(400, 590, 'ground');
 
-    emitter.startFollow(logo);
+    // Creating platforms
+    platforms.create(600, 400, 'platform');
+    platforms.create(50, 250, 'platform');
+    platforms.create(750, 220, 'platform');
+
+    // Creating a player warrior
+    player = this.physics.add.sprite(100, 450, 'player')
+    player.setBounce(0.2);
+    player.setCollideWorldBounds(true);
+    player.displayWidth = 48;
+    player.displayHeight = 48;
+
+
+    // Making the player and enemies to collide the platforms
+    this.physics.add.collider(player, ground);
+    this.physics.add.collider(enemy, ground);
+
+    // Animations for the player
+    this.anims.create(
+        {
+        key: 'left',
+        frames: this.anims.generateframeNumbers('player', { start: 35, end: 3})
+    })
+    
 }
 
 function update() {
