fa723f8b9e1241eafac6caf91fa4ce47fc8c4f79
Lionbull <geo1529@yahoo.com>
2022-10-19 18:11:57 +0300

    First map about to be ready

--- JS/index.js
+++ JS/index.js
@@ -8,7 +8,7 @@ const config = {
         mode: Phaser.Scale.FIT,
         parent: 'phaser-example',
         autoCenter: Phaser.Scale.CENTER_BOTH,
-        width: 800,
+        width: 1100,
         height: 600,
     },
 
@@ -40,6 +40,7 @@ function preload () {
 
     this.load.image("sky", "../assets/sky.png");
     this.load.image("platform", "../assets/platform.png")
+    this.load.image("wall", "../assets/wall.png")
     this.load.image("ground", "../assets/ground.png")
     this.load.spritesheet({
         key: 'warrior',
@@ -62,73 +63,163 @@ function preload () {
 function create () {
     this.add.image(400, 300, "sky");
 
+    // Creating platforms
+    createPlatforms.call(this);
+    
+
+    // Creating a player warrior
+    createPlayer.call(this);
+
+    // Annimations for the player
+    createPlayerAnimations.call(this);
+
+    // Allowing user to control the players
+    cursors = this.input.keyboard.createCursorKeys();
+
+
     enemy = this.physics.add.image(200, 100, "enemy");
     enemy.displayWidth = 48
     enemy.displayHeight = 48
 
-    enemy.setVelocity(100);
-    enemy.setBounce(0.2);
+    enemy.setVelocity(200);
+    enemy.setBounce(1, 0.2);
     enemy.setCollideWorldBounds(true)
+    enemy.setSize(20, 32);
 
-    // Creating the ground
+    
+    // Making the player and enemies to collide the platforms
+    this.physics.add.collider(player, platforms);
+    this.physics.add.collider(player, ground);
+    
+    this.physics.add.collider(enemy, platforms);
+    this.physics.add.collider(enemy, ground);
+
+    this.physics.add.collider(player, enemy, hitEnemy, null, this);
+    
+}
+
+function update()
+{
+    playerControl.call(this);
+}
+
+
+
+function collectSomething() {
+    console.log("Collected")
+}
+
+
+function hitEnemy() {
+    console.log("Hit!")
+}
+
+
+// Function for creating platforms
+function createPlatforms() {
     platforms = this.physics.add.staticGroup();
-
+    
+    // Creating the ground
     platforms.create(400, 590, 'ground').setScale(2).refreshBody();
 
-    // Creating platforms
-    platforms.create(600, 400, 'platform');
-    platforms.create(50, 250, 'platform');
-    platforms.create(750, 220, 'platform');
+    // Section 1
+    wall1_s1 = platforms.create(250, 310, 'wall');
+    wall1_s1.setSize(35, 500);
+    wall1_s1.displayHeight = 500;
 
-    // Creating a player warrior
-    player = this.physics.add.sprite(100, 450, 'warrior')
-    player.setBounce(0.2);
-    player.setCollideWorldBounds(true);
-    player.displayWidth = 48;
-    player.displayHeight = 48;
+    platform2_s1 = platforms.create(200, 250, 'platform');
+    platform2_s1 = platforms.create(200, 450, 'platform');
+    platform2_s1 = platforms.create(200, 250, 'platform');
 
-    // Animations for the player
-    this.anims.create(
+    platform3_s1 = platforms.create(50, 350, 'platform');
+
+    platform4_s1 = platforms.create(50, 150, 'platform');
+
+    // Section 2
+    platform1_s2 = platforms.create(600, 440, 'platform');
+    platform1_s2.setSize(400, 35);
+    platform1_s2.displayWidth = 400;
+
+    wall1_s2 = platforms.create(450, 180, 'wall');
+    wall1_s2.setSize(35, 500);
+    wall1_s2.displayHeight = 500;
+
+    wall2_s2 = platforms.create(800, 190, 'wall');
+    wall2_s2.setSize(35, 200);
+    wall2_s2.displayHeight = 200;
+
+    wall3_s2 = platforms.create(950, 310, 'wall');
+    wall3_s2.setSize(35, 500);
+    wall3_s2.displayHeight = 500;
+
+    platform2_s2 = platforms.create(930, 500, 'platform');
+    platform2_s2.setSize(10, 35);
+    platform2_s2.displayWidth = 10;
+
+    platform3_s2 = platforms.create(510, 200, 'platform');
+    platform3_s2.setSize(90, 35);
+    platform3_s2.displayWidth = 90;
+
+    dot1_s2 = platforms.create(650, 300, 'platform');
+    dot1_s2.setSize(5, 5);
+    dot1_s2.displayWidth = 5;
+    dot1_s2.displayHeight = 5;
+
+    wall4_s2 = platforms.create(550, 100, 'wall');
+    wall4_s2.setSize(10, 70);
+    wall4_s2.displayWidth = 10;
+    wall4_s2.displayHeight = 70;
+
+    dot2_s2 = platforms.create(630, 70, 'platform');
+    dot2_s2.setSize(10, 10);
+    dot2_s2.displayWidth = 10;
+    dot2_s2.displayHeight = 10;
+
+    dot2_s2 = platforms.create(710, 70, 'platform');
+    dot2_s2.setSize(10, 10);
+    dot2_s2.displayWidth = 10;
+    dot2_s2.displayHeight = 10;
+
+}
+
+
+// Function for creating players animations
+function createPlayerAnimations() {
+    this.anims.create(    
         {
         key: 'left',
-        frames: this.anims.generateframeNumbers('warrior', { start: 35, end: 45}),
-        frameRate: 20
+        frames: this.anims.generateFrameNumbers('warrior', { start: 20, end: 30}),
+        frameRate: 20,
+        repeat: -1
     })
 
     this.anims.create({
-        key: 'turn',
-        frames: [ { key:'warrior', frame: 42} ],
+        key: 'stand',
+        frames: this.anims.generateFrameNumbers('warrior', { start: 0, end: 19}),
         frameRate: 20
     })
 
     this.anims.create({
         key: 'right',
-        frames: this.anims.generateframeNumbers('warrior', { start: 35, end: 45}),
+        frames: this.anims.generateFrameNumbers('warrior', { start: 20, end: 30}),
         frameRate: 30
-    })
-
-    cursors = this.input.keyboard.createCursorKeys();
-
-    enemy = this.physics.add.group({
-        key: 'enemy',
-        repeat: 11,
-        setXY: { x:12, y: 0, stepX: 70 }
-    })
-
-    // Making the player and enemies to collide the platforms
-    this.physics.add.collider(player, platforms);
-    this.physics.add.collider(enemy, platforms);
-
-    this.physics.add.collider(player, enemy, hitEnemy, null, this);
+    });
 }
 
-function update()
-{
-    if (gameOver)
-    {
-        return;
-    }
 
+// Function for creating player
+function createPlayer() {
+    player = this.physics.add.sprite(800, 0, 'warrior')
+    player.setBounce(0.1);
+    player.setCollideWorldBounds(true);
+    player.displayWidth = 48;
+    player.displayHeight = 48;
+    player.setSize(15, 32);
+}
+
+
+// Function for controlling and animating player
+function playerControl() {
     if (cursors.left.isDown)
     {
         player.setVelocityX(-160);
@@ -145,19 +236,11 @@ function update()
     {
         player.setVelocityX(0);
 
-        player.anims.play('turn');
+        player.anims.play('stand');
     }
 
     if (cursors.up.isDown && player.body.touching.down)
     {
-        player.setVelocityY(-330);
+        player.setVelocityY(-275);
     }
-}
-
-function collectSomething() {
-    console.log("Collected")
-}
-
-function hitEnemy() {
-    console.log("Hit!")
 }
